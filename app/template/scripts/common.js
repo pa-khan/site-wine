4bd5b74042a279a08a4ae31ef223dc915d866cbe
e5ash <io@e5ash.com>
2017-11-04 01:58:52 +0300

    Layout 100% - desktop + mobile

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -11,4 +11,23 @@ $(document).ready(function($) {
 			$('body,html').animate({scrollTop: top}, 1000);
 		}
 	});
+
+	$('.catalog__button').click(function(event) {
+		var title = $(this).parents('.catalog__item').find('.catalog__subtitle').html();
+		// title.hide();
+		$('#popup-order .form__title').html('Заказ на дубовую щепу: <br />' + title);
+	});
+
+	$('.panel__hum').click(function(event) {
+		$(this).toggleClass('panel__hum_click');
+		$('.panel__nav').toggle();
+	});
+
+
+	$(window).resize(function(event) {
+		if ($(window).width() >= 1050) {
+			$('.panel__hum').removeClass('panel__hum_click');
+			$('.panel__nav').removeAttr('style');
+		}
+	});
 });
